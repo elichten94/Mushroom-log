eeefd3f57951881671d167c89817ce45a8f271f6
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-12-20 22:09:42 -0600

    Move pictures to assets folder

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
         <Heading id="title" as="h1" size="2xl">
           The Mushroom Log
         </Heading>
-        <img id="header-img" src={require("./mushroom-svgrepo-com.svg")} alt="mushroom.svg"/>
+        <img id="header-img" src={require("../assets/mushroom-svgrepo-com.svg")} alt="mushroom.svg"/>
         <Heading as="h3" id="map-banner">
           Scroll through the map to add your favorite spots!
         </Heading>
